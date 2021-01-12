b1e1027867bdc546045d5a76c46479727e9f440d
Nicola Paoli <nicolapaoli.work@gmail.com>
2021-01-12 18:36:14 +0100

    Create page layout

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const URL = "http://localhost:3000/tweets"
+
+const getTwitterData = () => {
+
+}
+
+const saveNextPage = (metadata) => {
+    
+}
+
+const selectTrend = (e) => {
+    
+}
+
+const nextPageButtonVisibility = (metadata) => {
+    
+}
+
+const buildTweets = (tweets, nextPage) => {
+
+}
+
+const buildImages = (mediaList) => {
+
+}
